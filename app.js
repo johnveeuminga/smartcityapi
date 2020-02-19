8400fa5566dc67539d378fff21f821fc78bf3ad4
John Vee Uminga <johnveeuminga@gmail.com>
2020-02-19 11:09:19 +0800

    API changes.

--- app.js
+++ app.js
@@ -3,37 +3,56 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose");
 
-mongoose.connect("mongodb://localhost/smart_city", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/smart_city", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var applicantSchema = new mongoose.Schema({
+var applicationSchema = new mongoose.Schema({
     age: String,
     sex: String,
     civilStatus: String,
     fname: String,
     lname: String,
     image: String,
+    status: Number,
+    address: String,
+    occupation: String,
+    workAddress: String,
+    date: String,
 });
 
-var Applicant = mongoose.model("Applicant", applicantSchema);
+var Application = mongoose.model("application", applicationSchema);
 
 
 app.get("/", function(req, res){
     res.render("application");
 });
 
-app.get("application", function(req, res){
-    Applicant.find({}, function(err, applicants){
-        if(err){
-            console.log("Err");
+app.get("/application", function(req, res){
+    Application.find({}, function(err, applications) {
+        if (err) {
+            console.log("Err")
+            res.status(500)
+            res.json("Something went wrong.")
         } else {
-            res.render("application", {applicants: applicants})
+            res.json({
+                applications,
+            })
         }
-    });
-});
+    })
+})
+ 
+// app.get("application", function(req, res){
+//     Application.find({}, function(err, Applications){
+//         if(err){
+//             console.log("Err");
+//         } else {
+//             res.render("application", {Applications: Applications})
+//         }
+//     });
+// });
 
 
 app.post("/application", function(req, res){
@@ -42,14 +61,61 @@ app.post("/application", function(req, res){
     var age = req.body.age;
     var sex = req.body.sex;
     var civilStatus = req.body.civilStatus;
-    var newApplicant = {fname: fname, lname: lname, age: age, sex: sex, civilStatus: civilStatus};
-    Applicant.create(newApplicant, function(err, newlyCreated){
+    var address = req.body.address;
+    var occupation = req.body.occupation;
+    var workAddress = req.body.workAddress;
+    var date = req.body.date;
+    var newApplication = {
+        fname: fname, 
+        lname: lname, 
+        age: age, 
+        sex: sex, 
+        civilStatus: civilStatus, 
+        status: 1, 
+        date: date, 
+        address: address, 
+        occupation: occupation, 
+        workAddress: workAddress
+    };
+    Application.create(newApplication, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
-            res.redirect("/");     
+            res.json({
+                application: newlyCreated
+            })
         }
     });
+})
+
+
+app.get('/application/:id', function(req, res) {
+    const id = req.params.id
+    Application.findById(id, function(err, application) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json({
+                application: application
+            })
+        }
+    })
+})
+
+app.put('/application/:id', function(req, res) {
+    const application = req.body
+
+    Application.findByIdAndUpdate(req.params.id, application, {
+        upsert: true
+    },function(err, newApplication) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json({
+                application: newApplication
+            })
+        }
+    })
 })
 
 app.listen("3000", function(){
